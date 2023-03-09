f143c5de4bf225e507ebf5cb261546a96431a0a3
Mahmudul Hasan <contact.mahmudul.h@gmail.com>
2023-03-09 20:06:07 +0600

    cart done

--- simple-cart.js
+++ simple-cart.js
@@ -1,16 +1,16 @@
 const shoppingCart = [
-    { name: 'shoe', price: 1200 },
-    { name: 'bag', price: 2400 },
-    { name: 'watch', price: 1400 },
-    { name: 'belt', price: 1000 },
-    { name: 'pant', price: 1100 }
+    { name: 'shoe', price: 1200, quantity: 1 },
+    { name: 'bag', price: 2400, quantity: 2 },
+    { name: 'watch', price: 1400, quantity: 3 },
+    { name: 'belt', price: 1000, quantity: 4 },
+    { name: 'pant', price: 1100, quantity: 1 }
 ]
 
 function total(bought) {
     let sum = 0;
     for (i = 0; i < bought.length; i++) {
         var item = bought[i];
-        sum = sum + item.price;
+        sum = sum + item.price * item.quantity;
         // var priceitem = bought[i].price;
         // console.log(priceitem);
     }
